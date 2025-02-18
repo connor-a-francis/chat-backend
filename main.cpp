a36bac292fcbc9af891b6b25e8207f3a19321a5b
Connor Francis <cfster03@gmail.com>
2025-02-18 14:48:45 -0500

    basic response

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,19 +16,37 @@ int makeSocket(struct addrinfo*);
 void enableReuse(int);
 void bindInitial(int, struct addrinfo*);
 void listenInitial(int);
-void acceptNewConns(int, sockaddr_storage, int);
+int acceptNewConns(int, sockaddr_storage, int);
 
 int main(int argc, char const* argv[]) {
     struct sockaddr_storage their_addr;
     struct addrinfo *res;
     int sockfd, new_fd;
+    int buffer_size = 1000;
+    char *buffer = (char*)malloc(buffer_size * sizeof(char));
+    memset(buffer, 0, buffer_size);
 
     getLocal(&res);
     sockfd = makeSocket(res);
     enableReuse(sockfd);
     bindInitial(sockfd, res);
     listenInitial(sockfd);
-    acceptNewConns(sockfd, their_addr, new_fd);
+    new_fd = acceptNewConns(sockfd, their_addr, new_fd);
+    int req_len = recv(new_fd, buffer, buffer_size, 0);
+
+    if (req_len == -1) {
+        perror("listening on socket");
+        exit(1);
+    }
+
+    if (req_len == 0) {
+        std::cout << "Connection closed!";
+    } else {
+        send(new_fd, buffer, req_len, 0);
+        std::cout << buffer;
+    }
+    
+    
     freeaddrinfo(res);
 
 }
@@ -47,8 +66,8 @@ void getLocal(struct addrinfo **res) {
 }
 
 int makeSocket(struct addrinfo *res) {
-    int sockfd;
-    if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
+    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd == -1) {
         perror("socket initialization");
         exit(1);
     }
@@ -77,11 +96,13 @@ void listenInitial(int sockfd) {
     }
 }
 
-void acceptNewConns(int sockfd, struct sockaddr_storage their_addr, int new_fd) {
+int acceptNewConns(int sockfd, struct sockaddr_storage their_addr, int new_fd) {
     socklen_t addr_size;
     addr_size = sizeof their_addr;
-    if (((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)) = -1)) {
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+    if (new_fd == -1) {
         perror("accepting new connections");
         exit(1);
     }
+    return new_fd;
 }
